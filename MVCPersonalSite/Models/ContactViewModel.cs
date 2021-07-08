2b3e52bded82da2971139bc3e999f37eb99af853
Danielle Smalley <smalley.danielle@gmail.com>
2021-07-08 00:59:31 -0500

    Console error on projects page fixed, styling updated in footer, header nav bar, home page language/skill icons added, linkedin & github links added to contact, project layout updated, responsiveness on home & contact page added for icons on smaller screens.

--- MVCPersonalSite/Models/ContactViewModel.cs
+++ MVCPersonalSite/Models/ContactViewModel.cs
@@ -9,17 +9,17 @@ namespace WebApplication1.Models
     public class ContactViewModel
     {
         //Annotations are done in []
-        [Required(ErrorMessage = "*Name is required")]
+        [Required(ErrorMessage = "Ope! Please enter your name")]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Ope! Please enter your email")]
         [DataType(DataType.EmailAddress)] //this has its own error/required message that displays built in
         public string Email { get; set; }
 
-        [Required (ErrorMessage ="Please enter a subject")]
+        [Required (ErrorMessage ="Ope! Please enter a subject")]
         public string Subject { get; set; }
 
-        [Required(ErrorMessage = "Please write your message here")]
+        [Required(ErrorMessage = "Ope! Please write your message here")]
         [UIHint("MultilineText")] //provides a text area in the UI
         public string Message { get; set; }
     }
